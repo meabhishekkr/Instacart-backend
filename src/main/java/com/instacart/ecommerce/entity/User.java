42e3889a1be82a8a2de99adfd1e6adb6720abcbd
Abhishek Kumar <contactmeabhishek.kr@gmail.com>
2026-01-31 19:51:59 +0530

    Update User entity

--- src/main/java/com/instacart/ecommerce/entity/User.java
+++ src/main/java/com/instacart/ecommerce/entity/User.java
@@ -1,32 +1,82 @@
 package com.instacart.ecommerce.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "users")
+@Data
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private String firstName;
+
+    @Column(nullable = false)
     private String lastName;
+
+    @Column(unique = true)
     private String phoneNumber;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private UserRole role = UserRole.CUSTOMER;
+
+    @Column(nullable = false)
     private Boolean enabled = true;
+
+    @Column(nullable = false)
     private Boolean accountNonLocked = true;
+
     private String emailVerificationToken;
+
     private LocalDateTime emailVerificationTokenExpiry;
+
     private Boolean emailVerified = false;
+
     private String passwordResetToken;
+
     private LocalDateTime passwordResetTokenExpiry;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<Address> addresses = new HashSet<>();
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     private Cart cart;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private Set<Order> orders = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private Set<Review> reviews = new HashSet<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
     private Set<Wishlist> wishlists = new HashSet<>();
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
+
     private LocalDateTime lastLoginAt;
+
     public enum UserRole {
         CUSTOMER,
         SELLER,
