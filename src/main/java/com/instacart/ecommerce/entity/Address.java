59a1ed4c437a849eda37636c842cd5f715aac5af
Abhishek Kumar <contactmeabhishek.kr@gmail.com>
2026-01-31 21:12:26 +0530

    Update Address entiry

--- src/main/java/com/instacart/ecommerce/entity/Address.java
+++ src/main/java/com/instacart/ecommerce/entity/Address.java
@@ -1,17 +1,48 @@
 package com.instacart.ecommerce.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "addresses")
+@Data
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @Column(nullable = false)
     private String fullName;
+
+    @Column(nullable = false)
     private String phoneNumber;
+
+    @Column(nullable = false)
     private String addressLine1;
+
     private String addressLine2;
+
+    @Column(nullable = false)
     private String city;
+
+    @Column(nullable = false)
     private String state;
+
+    @Column(nullable = false)
     private String country;
+
+    @Column(nullable = false)
     private String postalCode;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     private AddressType addressType;
+
+    @Column(nullable = false)
     private Boolean isDefault = false;
 
     public enum AddressType {
