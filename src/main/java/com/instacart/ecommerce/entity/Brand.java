fd19cb1e65674d422e401b622a2b38ebace26de8
Abhishek Kumar <contactmeabhishek.kr@gmail.com>
2026-01-31 21:16:38 +0530

    Update Brand entity

--- src/main/java/com/instacart/ecommerce/entity/Brand.java
+++ src/main/java/com/instacart/ecommerce/entity/Brand.java
@@ -1,13 +1,25 @@
 package com.instacart.ecommerce.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
 
+@Entity
+@Table(name = "brands")
 @Data
 public class Brand {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String name;
+
+    @Column(unique = true)
     private String slug;
+
     private String description;
+
     private String logoUrl;
+
     private Boolean isActive = true;
 }
