c2181e8e1dcf1d1f71b288de481e91c51ed2435e
Abhishek Kumar <contactmeabhishek.kr@gmail.com>
2026-02-03 20:38:41 +0530

    Update Wishlist entity

--- src/main/java/com/instacart/ecommerce/entity/Wishlist.java
+++ src/main/java/com/instacart/ecommerce/entity/Wishlist.java
@@ -1,10 +1,27 @@
 package com.instacart.ecommerce.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
 import java.time.LocalDateTime;
 
+@Entity
+@Table(name = "wishlists")
+@Data
 public class Wishlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id")
     private Product product;
+
+    @CreationTimestamp
     private LocalDateTime createdAt;
 }
