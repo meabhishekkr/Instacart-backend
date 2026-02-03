0011f27d3037b84049a2a63bf50c19df72cab40a
Abhishek Kumar <contactmeabhishek.kr@gmail.com>
2026-02-03 20:20:47 +0530

    Update Tag

--- src/main/java/com/instacart/ecommerce/entity/Tag.java
+++ src/main/java/com/instacart/ecommerce/entity/Tag.java
@@ -1,10 +1,22 @@
 package com.instacart.ecommerce.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "tags")
+@Data
 public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
     private String name;
+
+    @ManyToMany(mappedBy = "tags")
     private Set<Product> products = new HashSet<>();
 }
