206845778772d2747d2e44af8650eef8a4828d6c
Abhishek Kumar <contactmeabhishek.kr@gmail.com>
2026-02-03 12:46:08 +0530

    Update Category entity

--- src/main/java/com/instacart/ecommerce/entity/Category.java
+++ src/main/java/com/instacart/ecommerce/entity/Category.java
@@ -1,25 +1,57 @@
 package com.instacart.ecommerce.entity;
 
+import jakarta.persistence.*;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "categories")
 @Data
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false, unique = true)
     private String slug;
+
+    @Column(length = 1000)
     private String description;
+
     private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
     private Category parent;
+
+    @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
     private Set<Category> subCategories = new HashSet<>();
+
+    @OneToMany(mappedBy = "category")
     private Set<Product> products = new HashSet<>();
+
+    @Column(nullable = false)
     private Boolean isActive = true;
+
     private Integer displayOrder = 0;
+
     private String metaTitle;
+
     private String metaDescription;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
     private LocalDateTime updatedAt;
 }
